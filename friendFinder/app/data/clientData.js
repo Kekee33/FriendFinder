78569f07fe05b4fdada99464ba9a3af0f2d6640c
keisha <keisha@sample.com>
2017-08-07 12:58:04 -0400

    initial upload plus

---
+++ friendFinder/app/data/clientData.js
@@ -0,0 +1,16 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the client info.
+
+// ===============================================================================
+
+var clientData = [
+  {
+    clientName: "Sean Connery",
+    clientPhoto: "http://short-biography.com/wp-content/uploads/sean-connery/sean_connery.jpg",
+    scores: [1,2,3,4,5,]
+  }
+];
+
+
+module.exports = clientData;
