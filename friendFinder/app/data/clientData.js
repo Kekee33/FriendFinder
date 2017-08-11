3b5a8a556d2456ece3321656c506db068558c0b2
keisha <keisha@sample.com>
2017-08-11 12:30:55 -0400

    almost working

--- friendFinder/app/data/clientData.js
+++ friendFinder/app/data/clientData.js
@@ -4,13 +4,32 @@
 
 // ===============================================================================
 
-var clientData = [
-  {
-    clientName: "Sean Connery",
-    clientPhoto: "http://short-biography.com/wp-content/uploads/sean-connery/sean_connery.jpg",
-    scores: [1,2,3,4,5,]
-  }
-];
+var clientData = [{
+        "clientName": "Sean Connery",
+        "clientPhoto": "http://short-biography.com/wp-content/uploads/sean-connery/sean_connery.jpg",
+        "scores": [1, 2, 3, 4, 5, 4, 3, 2, 1, 5]
+    },{
+
+        "clientName": "Superman",
+        "clientPhoto": "http://1.bp.blogspot.com/-w9vnmSGyhBU/VRL-ioqskHI/AAAAAAAAATA/WpM7LR8Pg1k/s1600/superman01.png",
+        "scores": [2, 2, 5, 1, 3, 3, 3, 1, 5, 4]
+    },{
+
+        "clientName": "Shamar Moore",
+        "clientPhoto": "http://3.bp.blogspot.com/-Im6pOlLQMMA/TvSWRrXCa7I/AAAAAAAAAYg/M60ROcJqOsg/s1600/shemar-moore-1.jpg",
+        "scores": [3, 2, 5, 4, 1, 2, 5, 1, 1, 5]
+    },{
+
+        "clientName": "Pink Panther",
+        "clientPhoto": "http://3.bp.blogspot.com/-Yr-STvFRPkI/T_Av9DiWjJI/AAAAAAAAFzI/xqmAK5qurMs/s1600/pink-panther.jpg",
+        "scores": [1, 5, 3, 5, 2, 1, 4, 3, 2, 5]
+    },{
+
+        "clientName": "Ben Afleck",
+        "clientPhoto": "https://tse2.mm.bing.net/th?id=OIP.NjhJ0wRe7QE1D9-axpF6pwEsDh&pid=15.1&P=0&w=245&h=185",
+        "scores": [4, 5, 3, 5, 1, 4, 4, 3, 1, 2]
+    
+}];
 
 
 module.exports = clientData;
